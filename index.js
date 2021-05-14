877f2c905c6743599ff098e455508ac0700b0b82
Chip Thien <themacrochip@gmail.com>
2021-05-13 23:49:04 -0500

    Write google api creds into file from env var if on Heroku

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const Koa = require('koa');
 const Router = require('koa-router');
 const cors = require('@koa/cors');
+const fs = require('fs/promises');
 
 const app = new Koa();
 const router = new Router();
 
+createGoogleApiJson();
 require('./api').api(router)
 
 app
@@ -14,3 +16,11 @@ app
 const PORT = process.env.PORT || 8080;
 app.listen(PORT);
 console.log(`Server running on port ${PORT}`);
+
+async function createGoogleApiJson() {
+    try {
+        await fs.stat(env.process.GOOGLE_APPLICATION_CREDENTIALS);
+    } catch (e) {
+        await fs.writeFile(env.process.GOOGLE_APPLICATION_CREDENTIALS, env.process.GOOGLE_API_JSON);
+    }
+}
