2fcd7df9bd0a57d23f20e36c6a6068561c5b0eec
Chip Thien <themacrochip@gmail.com>
2021-05-13 23:58:01 -0500

    Add laser asset

--- api.js
+++ api.js
@@ -8,7 +8,7 @@ module.exports = {
         router.post('/laserify', koaBody({ multipart: true }), async (ctx, next) => {
             const baseImage = ctx.request.files.image.path;
             const outputFilename = Date.now().toString() + '.png';
-            const result = await core(baseImage, './images/laser-flare.webp', outputFilename);
+            const result = await core(baseImage, './laser-flare.webp', outputFilename);
             ctx.response.status = 200;
             ctx.attachment('laser-eyes.jpg');
             ctx.response.body = fs.createReadStream(outputFilename);
--- cli.js
+++ cli.js
@@ -1,5 +1,5 @@
 const core = require('./core.js').core;
 
 (async () => {
-    core('./images/headshot3.png', './images/laser-flare.webp', 'output.png')
+    core('./images/chip-and-liz.jpg', './laser-flare.webp', 'output.png')
 })()
