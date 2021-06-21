bf40cf69525421f4cacb26d87455a3b316054a9c
Chip Thien <themacrochip@gmail.com>
2021-06-20 21:29:54 -0500

    Laser scales to eye size but current laser asset has too much blank space at edge.

--- cli.js
+++ cli.js
@@ -1,5 +1,5 @@
 const { _core, core } = require('./core.js');
 
 (async () => {
-    _core('./images/chip-and-liz.jpg', './laser-flare.webp', 'output.png', require('./test.json'))
+    _core('./images/chip-and-liz.jpg', './images/red-square2.png', 'output.png', require('./test.json'))
 })()
--- core.js
+++ core.js
@@ -13,35 +13,38 @@ async function _core(faceFilename, laserEyeFilename, outputFilename, result) {
         throw "Couldn't find any faces";
     }
     console.log(JSON.stringify(faces, null, 2));
-    const baseImage = sharp(faceFilename);
-    const baseImageMetadata = await baseImage.metadata();
-    const imageSize = Math.min(baseImageMetadata.width, baseImageMetadata.height, 250);
-    const halfImageSize = imageSize / 2;
-    const laserEye = await sharp(laserEyeFilename)
-        .resize(imageSize, imageSize)
-        .toBuffer();
-    return baseImage
-        .composite(faces.map(face => eyesComposites(face, halfImageSize, laserEye)).flat())
+    return sharp(faceFilename)
+        .composite((await Promise.all(faces.map(face => eyesComposites(face.landmarks, laserEyeFilename)))).flat())
         .toFile(outputFilename);
 }
 
-function eyesComposites(face, halfImageSize, laserEye) {
-    const leftEyePosition = face.landmarks.find(l => l.type === "LEFT_EYE").position;
-    const rightEyePosition = face.landmarks.find(l => l.type === "RIGHT_EYE").position;
+async function eyesComposites(landmarks, laserEyeFilename) {
+    //this assumes both eyes are same size. that is bad assumption for i.e. angled faces
+    const eyeSize = parseFloat(getLandmark(landmarks, "LEFT_EYE_RIGHT_CORNER").x) - parseFloat(getLandmark(landmarks, "LEFT_EYE_LEFT_CORNER").x); //TODO: abs for upsidedown headS? have to see what api does
+    const halfImageSize = eyeSize / 2;
+    const laserEye = await sharp(laserEyeFilename)
+        .resize(Math.round(eyeSize), Math.round(eyeSize))
+        .toBuffer();
+    const leftEyePosition = getLandmark(landmarks, "LEFT_EYE");
+    const rightEyePosition = getLandmark(landmarks, "RIGHT_EYE");
     console.log(JSON.stringify(leftEyePosition, null, 2));
     console.log(JSON.stringify(rightEyePosition, null, 2));
     return [
         {
-            left: parseInt(leftEyePosition.x) - halfImageSize, //TODO: round instead of truncate
-            top: parseInt(leftEyePosition.y) - halfImageSize,
+            left: Math.round(parseFloat(leftEyePosition.x) - halfImageSize),
+            top: Math.round(parseFloat(leftEyePosition.y) - halfImageSize),
             input: laserEye
         },
         {
-            left: parseInt(rightEyePosition.x) - halfImageSize,
-            top: parseInt(rightEyePosition.y) - halfImageSize,
+            left: Math.round(parseFloat(rightEyePosition.x) - halfImageSize),
+            top: Math.round(parseFloat(rightEyePosition.y) - halfImageSize),
             input: laserEye
         }
     ];
+}
+
+function getLandmark(landmarks, type) {
+    return landmarks.find(l => l.type === type).position;
 }
 
 module.exports = {
