73c5b3d0fc6896b9f3eb456cbc598a16b786ac2a
Chip Thien <themacrochip@gmail.com>
2022-06-16 23:04:42 -0500

    wip hair sits on top

--- cli.js
+++ cli.js
@@ -1,6 +1,6 @@
 const { _core, core } = require('./core.js');
 
 (async () => {
-    _core('./images/chip-and-liz.jpg', './laser-flare.webp', 'output.png', require('./test.json'))
+    _core('./images/chip-and-liz.jpg', './images/slop-mop.png', 'output.png', require('./test.json'))
     // core('C:\\Users\\Chip\\Downloads\\index2.jpg', './laser-flare.webp', 'output.png')
 })()
--- core.js
+++ core.js
@@ -1,7 +1,7 @@
 const sharp = require('sharp')
 const vision = require('@google-cloud/vision');
 
-const laserSizeMultiplier = 8;
+const laserSizeMultiplier = 1.6;
 
 async function core(faceFilename, laserEyeFilename, outputFilename) {
     const client = new vision.ImageAnnotatorClient();
@@ -18,39 +18,32 @@ async function _core(faceFilename, laserEyeFilename, outputFilename, result) {
     const base = sharp(faceFilename);
     const baseMetadata = await base.metadata();
     return base
-        .composite((await Promise.all(faces.map(face => eyesComposites(face.landmarks, laserEyeFilename, baseMetadata)))).flat())
+        .composite((await Promise.all(faces.map(face => eyesComposites(face, laserEyeFilename, baseMetadata)))).flat())
         .toFile(outputFilename);
 }
 
-async function eyesComposites(landmarks, laserEyeFilename, baseMetadata) {
+async function eyesComposites(face, laserEyeFilename, baseMetadata) {
     //this assumes both eyes are same size. that is bad assumption for i.e. angled faces
-    const eyeSize = computeEyeSize(landmarks, baseMetadata);
-    const halfImageSize = eyeSize / 2;
+    const headSize = computeHeadSize(face.landmarks, baseMetadata);
+    const halfImageSize = headSize / 2;
     const laserEye = await sharp(laserEyeFilename)
-        .resize(eyeSize, eyeSize)
+        .resize(headSize, headSize)
         .toBuffer();
-    const leftEyePosition = getLandmark(landmarks, "LEFT_EYE");
-    const rightEyePosition = getLandmark(landmarks, "RIGHT_EYE");
-    console.log(JSON.stringify(leftEyePosition, null, 2));
-    console.log(JSON.stringify(rightEyePosition, null, 2));
+    const forehead = face.fdBoundingPoly.vertices[0];
+    // console.log(JSON.stringify(forehead, null, 2));
     return [
         {
-            left: Math.round(parseFloat(leftEyePosition.x) - halfImageSize),
-            top: Math.round(parseFloat(leftEyePosition.y) - halfImageSize),
-            input: laserEye
-        },
-        {
-            left: Math.round(parseFloat(rightEyePosition.x) - halfImageSize),
-            top: Math.round(parseFloat(rightEyePosition.y) - halfImageSize),
+            left: Math.round(parseFloat(forehead.x)),
+            top: Math.round(parseFloat(forehead.y) - halfImageSize),
             input: laserEye
         }
     ];
 }
 
-function computeEyeSize(landmarks, baseMetadata) {
-    let eyeSize = parseFloat(getLandmark(landmarks, "LEFT_EYE_RIGHT_CORNER").x) - parseFloat(getLandmark(landmarks, "LEFT_EYE_LEFT_CORNER").x); //TODO: abs for upsidedown headS? have to see what api does
-    eyeSize = eyeSize * laserSizeMultiplier;
-    return Math.round(Math.min(eyeSize, baseMetadata.width));
+function computeHeadSize(landmarks, baseMetadata) {
+    let headSize = parseFloat(getLandmark(landmarks, "RIGHT_EYE_RIGHT_CORNER").x) - parseFloat(getLandmark(landmarks, "LEFT_EYE_LEFT_CORNER").x); //TODO: abs for upsidedown headS? have to see what api does
+    headSize = headSize * laserSizeMultiplier;
+    return Math.round(Math.min(headSize, baseMetadata.width));
 }
 
 function getLandmark(landmarks, type) {
